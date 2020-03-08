1c7be2aa088aa3da1b7d259dc62e242fad61dfeb
Shreyas <dlshreyas30@gmail.com>
2020-03-08 20:51:23 +0530

    More TestCases added

--- polls/tests.py
+++ polls/tests.py
@@ -8,3 +8,15 @@ class QuestionModelTests(TestCase) :
     def test_was_published_future(self) :
         question = Question(pub_date = timezone.now()+datetime.timedelta(days=30))
         self.assertIs(question.was_published_recently(),False)
+
+    def test_past_question(self) :
+        now = timezone.now()
+        time = now - datetime.timedelta(days=1,seconds=1)
+        question = Question(pub_date = time)
+        self.assertIs(question.was_published_recently(),False)
+
+    def test_for_recent(self) :
+        now = timezone.now()
+        time = now - datetime.timedelta(hours=23,minutes=59,seconds=59)
+        question = Question(pub_date = time)
+        self.assertIs(question.was_published_recently(),True)
