814e4bdf266eee74c88de52b9d2668f1b8f353c9
Shreyas <dlshreyas30@gmail.com>
2020-03-08 20:37:09 +0530

    Automated testing added

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+import datetime
+from django.utils import timezone
+from .models import Question
 # Create your tests here.
+
+class QuestionModelTests(TestCase) :
+    def test_was_published_future(self) :
+        question = Question(pub_date = timezone.now()+datetime.timedelta(days=30))
+        self.assertIs(question.was_published_recently(),False)
