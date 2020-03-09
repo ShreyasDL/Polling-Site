1306507b44cf7883a073e4e135d88fbd1aa0c521
Shreyas <dlshreyas30@gmail.com>
2020-03-09 08:26:47 +0530

    View Testcases added

--- polls/tests.py
+++ polls/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 import datetime
 from django.utils import timezone
 from .models import Question
+from django.urls import reverse
 # Create your tests here.
 
 class QuestionModelTests(TestCase) :
@@ -20,3 +21,22 @@ class QuestionModelTests(TestCase) :
         time = now - datetime.timedelta(hours=23,minutes=59,seconds=59)
         question = Question(pub_date = time)
         self.assertIs(question.was_published_recently(),True)
+    
+def create_question(question_text,days) :
+    now = timezone.now()
+    date = now + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text,pub_date=date)
+
+class QuestionIndexViewTests(TestCase) :
+    def test_no_questions(self) :
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,"No polls available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'],[])
+    
+    def test_past_questions(self) :
+        question = create_question("Past Question.",days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question'],['<Question: Past Question.>'])
+    
+    def test_futu
